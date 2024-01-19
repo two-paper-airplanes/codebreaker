3f93f528c5cc99fa12146134694d74cbe9e48b71
priyankha! <109419209+two-paper-airplanes@users.noreply.github.com>
2024-01-19 17:54:08 +0800

    Create exam.cpp

---
+++ exam.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin >> x;
+	if (x == 1) {
+		cout << "Hello World";
+	} else if (x == 2) {
+		long long A,B;
+		cin >> A >> B;
+		cout << A*B;
+	}
+}
