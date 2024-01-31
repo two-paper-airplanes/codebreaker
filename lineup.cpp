c7439fa40e369e06432980b154bb63ef168c4e9a
priyankha! <109419209+two-paper-airplanes@users.noreply.github.com>
2024-01-31 12:17:05 +0800

    Modified 1 File

--- lineup.cpp
+++ lineup.cpp
@@ -1,42 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 #define pii pair<int, int>
 
-signed main() {
+int main() {
 	int n; cin>>n;
-	set<int> s;
-	multiset<int> ms;
 	vector<pii> v;
-	
+	set<int> s;
+	//make a vector of pairs containing the distance of each ot the cows and their types
 	for (int i=0; i<n; i++) {
-		int x,y; cin>>x>>y;
-		v.push_back({x,y});
-	} 
-	
+		int x, y; cin>>x>>y;
+		v.push_back({x, y});
+	}
+	//sort them in ascending order
 	sort(v.begin(), v.end(), [](pii a, pii b) {
 		return a.first < b.first;
 	});
-	
-	int l=0,h=0,min=INT_MAX,cost=0, size = 0;
-	while (h<v.size()-1) {
-		if (size==3){
-			cost=v[h].first-v[l].first;
-			ms.erase(ms.find(v[l].second));
-			s.insert(ms.begin(), ms.end());
-			size = s.size();
-			s.clear();
-			l+=1;
-			if (cost<min) {
-				min = cost;
-			}
+	//make the array B[] of n integers and set all integers to 9e18
+	int B[n];
+	for (int i=0; i<n; i++) B[i] = 9e18;
+	//set the lower and upper limits at 0
+	int l=0, h=0;
+	//actual algorithm!
+	while (h<n) {
+		for (int i=l; i<h; i++) {
+			s.insert(v[i].second);
+		}
+		if (s.size() == 3) {
+			int dist = v[h].first-v[l].first;
+			//check if dist is smaller than b[h]
+			//if so, change b[h] to dist
+			if (dist<B[h]) B[h] = dist;
+			//increase l pointer
+			l++;
 		} else {
-			h+=1;
-			ms.insert(v[h].second);
-			s.insert(ms.begin(), ms.end());
-			size = s.size();
-			s.clear();
+			//increase h pointer
+			h++
 		}
 	}
-	
-	cout<<min;
+
+	//finally sort the B[] array and output B[0]
+	sort(B, B+n);
+	cout<<B[0];
+
 }
+
+/*
+---------------------------------------PSEUDOCODE---------------------------------------
+✅ um so basically i want to make a vector of pairs containing the distance of each ot the cows and their types
+✅ and sort them in ascending order
+✅ then make the array B[] of n integers and set all integers to 9e18
+✅ after that i should set the lower and upper limits at 0
+add the types to a set
+and check for the size of the set
+if the set size is 3, i should change b[h] to the distance which should be v[h].first-v[l].first, then i should move the l pointer up and repeat
+if the set size is not 3, i should move the h pointer up and repeat
+✅ this shld go on while h is less than n (um problem!!! what if the lowest distance includes the last integer)
+oh and if the h is fixed and l is moved up and the set size is still 3 then you shld check if the new b[h] is smaller and only change it if its smaller
+finally sort the B[] array
+and output B[0];
+*/
